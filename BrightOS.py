2f88f2e5330e461fb7d746ec19fbe52fae2c4fff
Scarlett/Eight <bennettbashford@gmail.com>
2025-12-12 10:13:10 -0500

    Load scripts in addition to plugins

--- BrightOS.py
+++ BrightOS.py
@@ -7,9 +7,12 @@ from simple_plugin_loader import Loader
 
 
 pluginlist = os.listdir(f"C:\\Users\%USER%\Appdata\Local\BrightOS\Plugins")
+scriptlist = os.listdir(f"C:\\Users\%USER%\Appdata\Local\BrightOS\Scripts")
 # initialize the loader
 loader = Loader()
 
 # load your plugins
 plugins = loader.load_plugins("C:\\Users\%USER%\Appdata\Local\BrightOS\Plugins", pluginlist)
+scripts = loader.load_plugins("C:\\Users\%USER%\Appdata\Local\BrightOS\Scripts", scriptlist)
 print(plugins)
+print(scripts)
