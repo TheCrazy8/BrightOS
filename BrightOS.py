82f54f19eddac629c26bee64a9db05c9a9f5635b
Scarlett/Eight <bennettbashford@gmail.com>
2025-12-12 10:12:04 -0500

    Enhance BrightOS with plugin loading functionality
    Added os and tkinter imports, initialized plugin loader, and loaded plugins from specified directory.

--- BrightOS.py
+++ BrightOS.py
@@ -1,7 +1,15 @@
 import math
+import os
 import datetime
+import tkinter as tk
 import telemetrix_uno_r4_wifi
-#Will use arduino to control robot
+from simple_plugin_loader import Loader
 
-def main():
-  return("placeholder")
+
+pluginlist = os.listdir(f"C:\\Users\%USER%\Appdata\Local\BrightOS\Plugins")
+# initialize the loader
+loader = Loader()
+
+# load your plugins
+plugins = loader.load_plugins("C:\\Users\%USER%\Appdata\Local\BrightOS\Plugins", pluginlist)
+print(plugins)
