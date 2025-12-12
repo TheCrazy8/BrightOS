7b6cd0b39fd396db773fafdaed65487455cf3c45
Scarlett/Eight <bennettbashford@gmail.com>
2025-12-12 12:41:34 -0500

    Add custom theme and plugins for VitePress

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,26 @@
+import VPLTheme from '@lando/vitepress-theme-default-plus';
+import './custom.css'
+import 'viewerjs/dist/viewer.min.css';
+import imageViewer from 'vitepress-plugin-image-viewer';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
+import { useRoute } from 'vitepress';
+import vitepressBackToTop from 'vitepress-plugin-back-to-top'
+import 'vitepress-plugin-back-to-top/dist/style.css'
+
+export default {
+...VPLTheme,
+enhanceApp(ctx) {
+VPLTheme.enhanceApp(ctx);
+// Register global component (optional)
+ctx.app.component('vImageViewer', vImageViewer);
+  vitepressBackToTop({
+      // default
+      threshold:300
+    })
+},
+setup() {
+const route = useRoute();
+// Enable the plugin
+imageViewer(route);
+}
+};
