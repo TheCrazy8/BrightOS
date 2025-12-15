164d83cdc752bb450409a590db2428267ac2cc10
Scarlett/Eight <bennettbashford@gmail.com>
2025-12-15 12:22:04 -0500

    Update and rename config.mjs to config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,35 @@
+export default {
+    title: "B&C Official",
+    description: "The Blaze & Company official site.",
+    base: "/Blaze-And-Company-Official/",
+    themeConfig: {
+        siteTitle: "Blaze & Company",
+        footer: {
+            message: "Released under the CC BY-NC-ND 4.0 License.",
+            copyright: "Copyright © 2025-present TheCrazy8",
+        },
+        nav: [
+        ],
+        sidebar: [
+        ],
+        
+        lastUpdated: {
+              text: 'Updated at',
+              formatOptions: {
+                    dateStyle: 'full',
+                    timeStyle: 'medium'
+              }
+        },
+        search: {
+              provider: 'local'
+        },
+    },
+    head: [
+        ['link', { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }]
+    ],
+    vite: {
+        ssr: {
+            noExternal: ['@lando/vitepress-theme-default-plus']
+        }
+    }
+};
