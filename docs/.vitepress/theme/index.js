44cdb7a320071d22f4206cd787f31f68ced949c4
Scarlett/Eight <bennettbashford@gmail.com>
2025-12-16 16:00:36 -0500

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,30 +1,15 @@
 import VPLTheme from '@lando/vitepress-theme-default-plus';
-import { h } from 'vue';
 import 'viewerjs/dist/viewer.min.css';
 import imageViewer from 'vitepress-plugin-image-viewer';
 import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
 import { useRoute } from 'vitepress';
 import vitepressBackToTop from 'vitepress-plugin-back-to-top';
 import 'vitepress-plugin-back-to-top/dist/style.css';
-import {
-  NolebaseEnhancedReadabilitiesMenu,
-  NolebaseEnhancedReadabilitiesScreenMenu,
-} from '@nolebase/vitepress-plugin-enhanced-readabilities/client';
-
 import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css';
 import { NolebaseEnhancedReadabilitiesPlugin } from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
 
 export default {
   ...VPLTheme,
-
-  Layout: () =>
-    h(VPLTheme.Layout, null, {
-      // An enhanced readabilities menu for wider screens
-      'nav-bar-content-after': () => h(NolebaseEnhancedReadabilitiesMenu),
-      // An enhanced readabilities menu for narrower screens (usually smaller than iPad Mini)
-      'nav-screen-content-after': () => h(NolebaseEnhancedReadabilitiesScreenMenu),
-    }),
-
   enhanceApp(ctx) {
     // Call base theme enhanceApp if it exists
     VPLTheme.enhanceApp?.(ctx);
